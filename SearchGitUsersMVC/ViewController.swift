98745786ce642de9d6deba4079cdd443892a4be0
Leonir Deolindo <leonir.ad@gmail.com>
2020-03-07 10:51:24 -0300

    Initial Commit

---
+++ SearchGitUsersMVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchGitUsersMVC
+//
+//  Created by Leonir Alves Deolindo on 07/03/20.
+//  Copyright © 2020 Leonir Deolindo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
