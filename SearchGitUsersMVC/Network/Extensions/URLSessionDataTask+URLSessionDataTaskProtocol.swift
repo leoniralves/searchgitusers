b37d8c545f3fda05671fc256a5c0effaaa027b95
Leonir Deolindo <leonir.ad@gmail.com>
2020-03-09 17:55:15 -0300

    update files

---
+++ SearchGitUsersMVC/Network/Extensions/URLSessionDataTask+URLSessionDataTaskProtocol.swift
@@ -0,0 +1,11 @@
+//
+//  URLSessionDataTask+URLSessionDataTaskProtocol.swift
+//  SearchGitUsersMVC
+//
+//  Created by Leonir Alves Deolindo on 07/03/20.
+//  Copyright © 2020 Leonir Deolindo. All rights reserved.
+//
+
+import Foundation
+
+extension URLSessionDataTask: URLSessionDataTaskProtocol {}
---
+++ SearchGitUsersMVC/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SearchGitUsersMVC
+//
+//  Created by Leonir Alves Deolindo on 07/03/20.
+//  Copyright © 2020 Leonir Deolindo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        let session = SessionProvider()
+//        session.request(type: User.self, service: UserService.query("")) { (response) in
+//
+//        }
+    }
+
+
+}
+
