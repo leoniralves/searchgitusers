f31cebc369d52191d373b1f674ea1521fcce45be
Leonir Deolindo <leonir.ad@gmail.com>
2020-03-10 17:41:02 -0300

    Remove Main.storyboard and create new nav controller

--- SearchGitUsersMVC/Application/Home/Controllers/HomeViewController.swift
+++ SearchGitUsersMVC/Application/Home/Controllers/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  SearchGitUsersMVC
 //
 //  Created by Leonir Alves Deolindo on 07/03/20.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
