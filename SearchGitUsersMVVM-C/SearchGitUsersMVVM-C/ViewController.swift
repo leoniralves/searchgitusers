089ba8cad299776d3f3fde97c395b57353ad0c87
Leonir Deolindo <leonir.ad@gmail.com>
2020-10-09 09:46:13 -0300

    initial commit from MVVM-C architecture

---
+++ SearchGitUsersMVVM-C/SearchGitUsersMVVM-C/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchGitUsersMVVM-C
+//
+//  Created by Leonir Alves Deolindo on 09/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
